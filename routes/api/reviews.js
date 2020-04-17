c2331064e265b0cd5cf71fccc0a41d99a09c3c0b
ryanlweston <ryanlweston@gmail.com>
2020-04-17 18:58:13 +0100

    validation began

--- routes/api/reviews.js
+++ routes/api/reviews.js
@@ -1,17 +1,21 @@
 const express = require('express');
 const router = express.Router();
+const jwt = require('jsonwebtoken');
+const { check, validationResult } = require('express-validator');
 const auth = require('../../middleware/auth');
 const reviews = require('../../models/Review');
 
 // @route    GET api/reviews
 // @desc     Test route
 // @access   Public
-router.post('/', auth, async (req, res) => {
+router.post(
+  '/',
+  [check('parent_id', 'University is required').not().isEmpty()],
+  auth,
+  async (req, res) => {
     try {
-        
-    } catch(err){
-        
-    }
-});
+    } catch (err) {}
+  }
+);
 
 module.exports = router;
