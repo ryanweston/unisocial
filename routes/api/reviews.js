4da94c8fb6358552a22abe833fd7c594492ad0a0
ryanlweston <ryanlweston@gmail.com>
2020-04-18 21:21:42 +0100

    review saving + check if user already has a review

--- routes/api/reviews.js
+++ routes/api/reviews.js
@@ -1,47 +1,49 @@
 const express = require('express');
 const router = express.Router();
-const jwt = require('jsonwebtoken');
 const { check, validationResult } = require('express-validator');
 const auth = require('../../middleware/auth');
 const Review = require('../../models/Review');
 const User = require('../../models/User');
 const University = require('../../models/University');
 
-
 // @route    GET api/reviews
 // @desc     Test route
 // @access   Public
-router.post(
-  '/', auth,
-  async (req, res) => {
-    try {
-    
-    const {internet, happiness, nightLife} = req.body.scores;
-    console.log(req.body.scores);
+router.post('/', auth, async (req, res) => {
+  try {
+    let user = await User.findById(req.user.id, '_id university');
 
-    const user = await User.findById(req.user.id, '_id university');
+    // Check if user already has a saved review.
+    // find() returns array as opposed to findOne which returns a string, hence .length check
+    const reviewStatus = await Review.find({ user_id: user.id });
+
+    if (reviewStatus.length) {
+      return res
+        .status(400)
+        .json({ errors: [{ msg: 'You have already submitted a review' }] });
+    }
 
     const review = new Review({
       university: user.university,
       user_id: user.id,
-      scores: [{
-        internet,
-        happiness,
-        nightLife,
-      }]
-    }); 
+      scores: [
+        {
+          internet: req.body.scores.internet,
+          happiness: req.body.scores.happiness,
+          nightlife: req.body.scores.nightlife,
+        },
+      ],
+    });
 
-    
-    await review.save()
-    console.log(review);  
+    await review.save();
+    res.json({ review });
 
-    
     //After review is submitted update university with average scores
 
     //Aggregate reviews, work out average scores and save in variable
     //  const newAverages = await Review.aggregate(
     //   [ { $match : { university_id : req.university_id } } ]
-    //  ); 
+    //  );
 
     //  console.log(newAverages);
 
@@ -50,18 +52,16 @@ router.post(
     //    { "university_id": university_id },
     //    { $average }
     //  )
-
-    } catch (err) {
-      //Error will be server issue if caught
-      console.error(err.message);
-      res.status(500).send('Server Error');
-    }
+  } catch (err) {
+    //Error will be server issue if caught
+    console.error(err.message);
+    res.status(500).send('Server Error');
   }
-);
+});
 
 // router.get('/', async (req, res) => {
 //   try {
-    
+
 //   } catch(err) {
 
 //   }
