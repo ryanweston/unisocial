6eddb9f5b663ab7b093f665a0e8d1d0c39101314
ryanlweston <ryanlweston@gmail.com>
2020-04-30 22:12:02 +0100

    uni validation + uni dropdown completed

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -1,9 +1,8 @@
-import React, { Fragment, useState } from 'react';
+import React, { Fragment, useState, useEffect } from 'react';
 import axios from 'axios';
 import UniversityList from './UniversityList';
 
 const Register = () => {
-
     // use state initialises objects default format. hook runs function on the given object
     const [formData, setFormData] = useState({
         name: '',
@@ -12,28 +11,80 @@ const Register = () => {
         passwordConfirm: ''
     });
 
+    // state = {
+    //     loading: true,
+    //     options: [null],
+    //     selected: ''
+    // }
+
+    const [dropdown, setDropdown] = useState({
+        loading: true,
+        options: [null],
+        selected: ''
+    })
+
+    //Will run error declaring missing dependacy, however I only want the function to run once after render,
+    //so warning should be ignored.
+
+    useEffect(() => {
+        const fetchList = async () => {
+            try {
+                const response = await axios.get('/api/university');
+                // Recieved errors in get reponse, had to convert to string to place in JS object array.
+                const string = JSON.stringify(response.data.list);
+                const uni = JSON.parse(string);
+                // Fills options with JSON request then sets loading to false which can be checked against
+                setDropdown({ selected: '', options: uni, loading: false });
+            } catch (err) {
+            }
+        }
+        fetchList();
+    }, []);
+
+    const universityCheck = [];
+    function universityChecker() {
+        for (var i = 0; i < dropdown.options.length; i++) {
+            var obj = dropdown.options[i]._id;
+            universityCheck.push(obj);
+        }
+    }
+
+    // Checks agains the amount of times the uni data is called
+    // if (dropdown.loading === false) {
+    //     console.log(dropdown);
+    // }
+
+
+
     const { name, email, password, passwordConfirm } = formData;
+    const university = dropdown.selected;
+
 
     // function runs from event, changing relevant form data for each input by selected name and it's corresponding value
     // ... operator preserves previous data from form, only changing states of given input
     const formChange = e => setFormData({ ...formData, [e.target.name]: e.target.value });
-
+    const dropdownChange = e => setDropdown({ ...dropdown, selected: e.target.value });
 
     const formSubmit = async e => {
         //Prevents default HTML handling, in this instance causing the page to refresh.
         e.preventDefault();
 
+        universityChecker();
+
         if (password !== passwordConfirm) {
             console.log('Passwords do not match');
+        } else if (universityCheck.indexOf(university) === -1) {
+            console.log('Select your university' + universityCheck);
         } else {
-
-            console.log(formData);
             //Creates object using variable that stored the form data
             const registerUser = {
                 name,
                 email,
-                password
+                password,
+                university
             }
+
+            console.log(registerUser);
             try {
                 //Sets headers for the post
                 const config = {
@@ -50,7 +101,6 @@ const Register = () => {
                 console.error(err.response.data);
             }
         }
-
     }
 
     return (
@@ -92,7 +142,7 @@ const Register = () => {
                 />
                 <input type="submit" />
             </form>
-            <UniversityList />
+            <UniversityList onValueChange={dropdownChange} state={dropdown} />
         </Fragment>
     );
 };
--- client/src/components/auth/UniversityList.js
+++ client/src/components/auth/UniversityList.js
@@ -1,47 +1,54 @@
 import React from 'react';
-import axios from 'axios';
+// import axios from 'axios';
 
 // Move data fetching to parent component and pass data down through the use of props
 // Research into redux store for state management
 
 class UniversityList extends React.Component {
 
-    state = {
-        loading: true,
-        options: [null]
-    }
+    // state = {
+    //     loading: true,
+    //     options: [null],
+    //     selected: ''
+    // }
 
     // componentDidMount function is called once after all the components and subcomponents have been rendered
     // best to make api calls within this react function as given component will have alread been mounted
     // and everything will be available to the DOM.
-    async componentDidMount() {
-        const response = await axios.get('/api/university');
-        // Recieved errors in get reponse, had to convert to string to place in JS object array.
-        const string = JSON.stringify(response.data.list);
-        const uni = JSON.parse(string);
-        // Fills options with JSON request then sets loading to false which can be checked against
-        this.setState({ options: uni, loading: false });
-        console.log(this.state.options);
-    }
+    // async componentDidMount() {
+    //     const response = await axios.get('/api/university');
+    //     // Recieved errors in get reponse, had to convert to string to place in JS object array.
+    //     const string = JSON.stringify(response.data.list);
+    //     const uni = JSON.parse(string);
+    //     // Fills options with JSON request then sets loading to false which can be checked against
+    //     this.setState({ options: uni, loading: false });
+    //     console.log(this.state.options);
+    // }
 
 
     render() {
+        // if (this.props.state.loading === false) {
+        //     console.log(this.props.state);
+        // }
         return (
+
             <div>
-                {this.state.loading || !this.state.options ? (
+                {this.props.state.loading || !this.props.state.options ? (
                     <div>loading...</div>
                 ) : (<div>
-                    <select>{
+                    <label>University</label>
+                    <select onChange={e => this.props.onValueChange(e)}>{
                         // map takes in the recieved university data, for each producing an option for the dropdown
-                        this.state.options.map((obj) => {
+                        this.props.state.options.map((obj) => {
                             return <option value={obj._id}>{obj.name}</option>
                         }
                         )
                     }</select>
-                </div>)}
+                </div>)
+                }
 
 
-            </div>
+            </div >
         )
     }
 }
