8f3614ddd94942b715d5efb1facdfd2937b85b05
ryanlweston <ryanlweston@gmail.com>
2020-04-30 22:12:12 +0100

    basic data fetch for unis

--- client/src/components/template/Landpage.js
+++ client/src/components/template/Landpage.js
@@ -1,14 +1,40 @@
 import React from 'react'
-import { Link } from 'react-router-dom';
+import axios from 'axios';
 
-const Landpage = () => {
-    return (
-        <section className="landing">
-            <h1> Landing </h1>
-            {/* <Link to='/register'>Register</Link>
-            <Link to='/login'>Login</Link> */}
-        </section>
-    )
+class Landpage extends React.Component {
+
+    state = {
+        universities: []
+    }
+
+    async componentDidMount() {
+        const res = await axios.get('api/reviews');
+        const string = JSON.stringify(res.data.reviews);
+        const reviews = JSON.parse(string);
+        this.setState({ universities: reviews });
+        console.log(this.state.universities[1].scores);
+    }
+
+    render() {
+        return (
+            <section className="landing">
+
+                {this.state.universities.map((obj) => {
+                    return <div>
+                        <div>{obj.name}</div>
+
+                        {obj.scores.map((points) => {
+                            return <div>
+                                <div> {points.total} </div>
+                            </div>
+                        }
+                        )}
+                    </div>
+                })}
+
+            </section>
+        )
+    }
 }
 
 export default Landpage;
