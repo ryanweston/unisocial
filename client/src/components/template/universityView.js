674031fe2532bed7879f1d60a975ca217df4676d
ryanlweston <ryanlweston@gmail.com>
2020-05-01 01:17:49 +0100

    university modal view began

---
+++ client/src/components/template/universityView.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class universityView extends React.Component {
+    render() {
+        return (
+            <div className="overlay">
+
+                <h1>cool!</h1>
+            </div>
+        )
+    }
+}
+
+export default universityView;
