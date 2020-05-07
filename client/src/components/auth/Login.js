b8aab6f9417ed3741fd32c54caa7733d5ba4503f
ryanlweston <ryanlweston@gmail.com>
2020-05-07 23:13:36 +0100

    removes login page when logged in

--- client/src/components/auth/Login.js
+++ client/src/components/auth/Login.js
@@ -1,10 +1,10 @@
-import React, { useState, onChange } from 'react'
+import React, { useState, onChange, Fragment } from 'react'
 import { connect } from 'react-redux'
 import { login } from '../../actions/login';
 import PropTypes from 'prop-types';
 import ReCAPTCHA from 'react-google-recaptcha'
 
-const Login = ({ login }) => {
+const Login = ({ login, auth, loading }) => {
 
     console.log(localStorage);
 
@@ -35,25 +35,36 @@ const Login = ({ login }) => {
     }
 
     return (
-        <section className="login" onSubmit={e => loginSubmit(e)}>
-            <form>
-                <label>Email</label>
-                <input name="email" type="email" onChange={(e) => changeState(e)} />
-                <label>Password</label>
-                <input name="password" type="password" onChange={(e) => changeState(e)} />
-                <ReCAPTCHA
-                    ref={recaptchaRef}
-                    sitekey="6Le3m_MUAAAAAGGupKFXSTuNEIBjwAB486DNz6NY"
-                    onChange={onChange}
-                />
-                <input type="submit" />
-            </form>
-        </section>
+        <Fragment>
+            {(auth && !loading ? (<div>
+                <h1>You're already logged in!</h1>
+            </div>) : (<section className="login" onSubmit={e => loginSubmit(e)}>
+                <form>
+                    <label>Email</label>
+                    <input name="email" type="email" onChange={(e) => changeState(e)} />
+                    <label>Password</label>
+                    <input name="password" type="password" onChange={(e) => changeState(e)} />
+                    <ReCAPTCHA
+                        ref={recaptchaRef}
+                        sitekey="6Le3m_MUAAAAAGGupKFXSTuNEIBjwAB486DNz6NY"
+                        onChange={onChange}
+                    />
+                    <input type="submit" />
+                </form>
+            </section>))}
+        </Fragment>
     )
+}
+
+const mapStateToProps = state => {
+    return {
+        auth: state.register.isAuthenticated,
+        loading: state.register.loading,
+    }
 }
 
 Login.propTypes = {
     login: PropTypes.func.isRequired,
 }
 
-export default connect(null, { login })(Login);
+export default connect(mapStateToProps, { login })(Login);
