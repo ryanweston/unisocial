d0b1dd873cfde28a074ec76f63a83f5b20f314ea
ryanlweston <ryanlweston@gmail.com>
2020-05-11 17:40:23 +0100

    images for university landpage

--- client/src/components/template/images.js
+++ client/src/components/template/images.js
@@ -1,6 +1,19 @@
 const images = [
-    { id: "default", src: './images/universities/default.png' },
-    { id: "aberdeen", src: './images/universities/aberdeen.jpg' }
+    { id: "default", src: './images/universities/aberdeen.jpg' },
+    { id: "aberdeen", src: './images/universities/aberdeen.jpg' },
+    { id: "plymouth", src: './images/universities/plymouth.jpg' },
+    { id: "wales", src: './images/universities/wales.jpg' },
+    { id: "dundee", src: './images/universities/dundee.jpg' },
+    { id: "agakhan", src: './images/universities/agakhan.jpg' },
+    { id: "angliaruskin", src: './images/universities/angliaruskin.jpg' },
+    { id: "gloucester", src: './images/universities/gloucester.jpg' },
+    { id: "birmingham", src: './images/universities/birmingham.jpg' },
+    { id: "aston", src: './images/universities/aston.png' },
+    { id: "aiul", src: './images/universities/aiul.jpg' },
+    { id: "bath", src: './images/universities/bath.jpg' },
+    { id: "birkbeck", src: './images/universities/birkbeck.jpg' },
+    { id: "bristol", src: './images/universities/bristol.jpg' },
+
 ]
 
 export default images;
