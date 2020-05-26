2b4c240e41c69552028b7e9cf305a51c343971d8
ryanlweston <ryanlweston@gmail.com>
2020-05-26 17:38:49 +0100

    additional comments

--- client/src/components/general/Alert.js
+++ client/src/components/general/Alert.js
@@ -4,15 +4,19 @@ import { connect } from 'react-redux';
 
 
 //Passing in props that have been mapped to the global redux state, specifically the alert
-const Alert = ({ alerts }) =>
-    alerts !== null &&
-    alerts.length > 0 &&
-    alerts.map(alert => (
-        <div key={alert.id} className={`alert alert-${alert.alertType}`}>
-            {alert.msg}
-        </div>
-    ));
-
+const Alert = ({ alerts }) => {
+    return (
+        //Checks against alerts array containing at least one index
+        alerts !== null &&
+        alerts.length > 0 &&
+        alerts.map(alert => (
+            //Set alert DOM
+            <div key={alert.id} className={`alert alert-${alert.alertType}`}>
+                {alert.msg}
+            </div>
+        ))
+    )
+}
 Alert.propTypes = {
     alerts: PropTypes.array.isRequired
 };
