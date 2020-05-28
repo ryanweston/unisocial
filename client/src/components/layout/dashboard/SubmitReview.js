7ccd0e017ce79dd6c44e706ad313bd88ea3e00a9
ryanlweston <ryanlweston@gmail.com>
2020-05-28 02:44:36 +0100

    condensed code + consistent colour ranges

--- client/src/components/layout/dashboard/SubmitReview.js
+++ client/src/components/layout/dashboard/SubmitReview.js
@@ -4,6 +4,7 @@ import { postReview } from '../../../actions/dashboard';
 import '../../../styles/Reviews.css';
 import '../../../styles/Buttons.css';
 import { Redirect } from 'react-router-dom';
+import emojis from '../../general/emojis'
 
 const SubmitReview = ({ postReview, submission }) => {
 
@@ -29,17 +30,6 @@ const SubmitReview = ({ postReview, submission }) => {
     const changeValue = e => setFormData({ ...formData, [e.target.name]: e.target.value });
 
     //Sets value parameters for slider colour changes
-    const changeColour = e => {
-        if (e.target.value >= 0) {
-            e.target.className = 'red'
-        }
-        if (e.target.value >= 1.5) {
-            e.target.className = 'orange'
-        }
-        if (e.target.value >= 3.5) {
-            e.target.className = 'green'
-        }
-    }
 
     console.log(formData);
 
@@ -56,70 +46,24 @@ const SubmitReview = ({ postReview, submission }) => {
             </Fragment>) : (
                     <div className="fullPage margin-reset">
                         <form onSubmit={e => onSubmit(e)} className="scoresContainer submission">
-                            <div className="item">
-                                <label><span role="img" aria-label="sheep">🍻</span> Nightlife</label>
-                                <input name="nightlife" className='default' type="range" min="0.25" max="5" step="0.25" onChange={e => { changeValue(e); changeColour(e) }} />
-                            </div>
-                            <div className="item">
-                                <label><span role="img" aria-label="sheep">😁</span> Happiness</label>
-                                <input name="happiness" className='default' type="range" min="0.25" max="5" step="0.25" onChange={e => { changeValue(e); changeColour(e) }} />
-                            </div>
-                            <div className="item">
-                                <label><span role="img" aria-label="sheep">👨‍🎓</span> Societies</label>
-                                <input name="societies" className='default' type="range" min="0.25" max="5" step="0.25" onChange={e => { changeValue(e); changeColour(e) }} />
-                            </div>
-                            <div className="item">
-                                <label><span role="img" aria-label="sheep">⚽</span> Sports</label>
-                                <input name="sports" className='default' type="range" min="0.25" max="5" step="0.25" onChange={e => { changeValue(e); changeColour(e) }} />
-                            </div>
-                            <div className="item">
-                                <label><span role="img" aria-label="sheep">🗣</span> Freedom Of Speech</label>
-                                <input name="freedom_of_speech" className='default' type="range" min="0.25" max="5" step="0.25" onChange={e => { changeValue(e); changeColour(e) }} />
-                            </div>
-                            <div className="item">
-                                <label><span role="img" aria-label="sheep">🎭</span> Culture</label>
-                                <input name="culture" className='default' type="range" min="0.25" max="5" step="0.25" onChange={e => { changeValue(e); changeColour(e) }} />
-                            </div>
-                            <div className="item">
-                                <label><span role="img" aria-label="sheep">☁</span> Weather</label>
-                                <input name="weather" className='default' type="range" min="0.25" max="5" step="0.25" onChange={e => { changeValue(e); changeColour(e) }} />
-                            </div>
-                            <div className="item">
-                                <label><span role="img" aria-label="sheep">🌈</span> LGBTQ+ Friendly</label>
-                                <input name="LGBTQ_friendly" className='default' type="range" min="0.25" max="5" step="0.25" onChange={e => { changeValue(e); changeColour(e) }} />
-                            </div>
-                            <div className="item">
-                                <label><span role="img" aria-label="sheep">🚨</span> Crime</label>
-                                <input name="crime" className='default' type="range" min="0.25" max="5" step="0.25" onChange={e => { changeValue(e); changeColour(e) }} />
-                            </div>
-                            <div className="item">
-                                <label><span role="img" aria-label="sheep">🌐</span> Internet</label>
-                                <input name="internet" className='default' type="range" min="0.25" max="5" step="0.25" onChange={e => { changeValue(e); changeColour(e) }} />
-                            </div>
-                            <div className="item">
-                                <label><span role="img" aria-label="sheep">🧠</span> Mental Health</label>
-                                <input name="mental_health" className='default' type="range" min="0.25" max="5" step="0.25" onChange={e => { changeValue(e); changeColour(e) }} />
-                            </div>
-                            <div className="item">
-                                <label><span role="img" aria-label="sheep">🌃</span> Student Events</label>
-                                <input name="student_events" className='default' type="range" min="0.25" max="5" step="0.25" onChange={e => { changeValue(e); changeColour(e) }} />
-                            </div>
-                            <div className="item">
-                                <label><span role="img" aria-label="sheep">🌳</span> Nature</label>
-                                <input name="nature" className='default' type="range" min="0.25" max="5" step="0.25" onChange={e => { changeValue(e); changeColour(e) }} />
-                            </div>
-                            <div className="item">
-                                <label><span role="img" aria-label="sheep">🌎</span> Diversity</label>
-                                <input name="diversity" className='default' type="range" min="0.25" max="5" step="0.25" onChange={e => { changeValue(e); changeColour(e) }} />
-                            </div>
-                            <div className="item">
-                                <label><span role="img" aria-label="sheep">🏠</span> Accomodation</label>
-                                <input name="accomodation" className='default' type="range" min="0.25" max="5" step="0.25" onChange={e => { changeValue(e); changeColour(e) }} />
-                            </div>
-                            <div className="item">
-                                <label><span role="img" aria-label="sheep">💸</span> Cost Of Living</label>
-                                <input name="cost_of_living" className='default' type="range" min="0.25" max="5" step="0.25" onChange={e => { changeValue(e); changeColour(e) }} />
-                            </div>
+
+                            {
+                                // Loops & maps score values from object 
+                                Object.keys(formData).map(key => {
+                                    let string = key;
+                                    let labelKey = string.replace(/_/g, " ");
+                                    const emoji = emojis.filter(emojis => emojis.type === key);
+
+                                    //Round value
+                                    let val = parseFloat(formData[key]).toFixed(0);
+
+                                    //Checks against key being total, as total is displayed below in seperate code block
+
+                                    return <div className="item">
+                                        <label><span role="img" aria-label={emoji[0].label}>{emoji[0].emoji}</span> {labelKey} <span className="editNum">{formData[key]}</span></label>
+                                        <input name={key} className='default' target={"_" + val} type="range" min="0.25" max="5" step="0.25" value={formData[key]} onChange={e => { changeValue(e) }} />
+                                    </div>
+                                })}
 
                             <div className="full">
                                 <button className="black top-margin" type="submit"><i className="fas fa-check"></i> Submit</button>
